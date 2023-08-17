8d936f0def7244d65bd79d6f17f1ca71103a14a6
AdoniTzadeq <neenakmathewk@gmail.com>
2023-08-17 19:36:36 +0530

    packagejson change

--- app.js
+++ app.js
@@ -12,9 +12,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
 // Able to receive data from the frontend to the backend
-
 const api = require('./routes/api');
 app.use('/api', api);
+
+const path = require('path');
+app.use(express.static('./dist/frontend'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist//frontend/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
